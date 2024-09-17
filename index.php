686cddc6738b4f2b9e61c6a3702f56f326969846
B0G0N1 <michelebogoni2001@gmail.com>
2024-09-17 21:33:33 +0200

    [FEAT] Add getInfo function

--- index.php
+++ index.php
@@ -7,11 +7,15 @@ class Movie {
         $this->title = $title;
         $this->year = $year;
     }
+
+    public function getInfo() {
+        return "{$this->title} ({$this->year})";
+    }
 }
 
 $movie1 = new Movie("Inception", 2010);
 $movie2 = new Movie("The Matrix", 1999);
 
-echo $movie1->title . " (" . $movie1->year . ")<br>";
-echo $movie2->title . " (" . $movie2->year . ")<br>";
+echo $movie1->getInfo() . "<br>";
+echo $movie2->getInfo() . "<br>";
 ?>
